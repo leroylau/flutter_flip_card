a122f0aa10d402b64de93c311ff44e84ad5ff366
william <luroylau@163.com>
2019-04-11 16:39:48 +0800

    Set font to use fonts in assets

--- lib/widgets/card_view.dart
+++ lib/widgets/card_view.dart
@@ -1,12 +1,53 @@
+import 'package:flip_card/models/card.dart';
 import 'package:flutter/material.dart';
 
 class CardView extends StatelessWidget {
-  final Card card;
+  final CardModel card;
+  final double parallaxPercent;
 
-  const CardView({Key key, this.card}) : super(key: key);
+  const CardView({Key key, this.card, this.parallaxPercent}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        // backgroud using the image
+        ClipRRect(
+          borderRadius: BorderRadius.circular(10.0),
+          child: Container(
+            child: FractionalTranslation(
+              translation: Offset(parallaxPercent * 2.0, 0.0),
+              child: OverflowBox(
+                maxWidth: double.infinity,
+                child: Image.asset(
+                  card.backdropAssetPath,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+        ),
+
+        // Text content
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 30.0, left: 20.0),
+              child: Text(
+                '${card.address}'.toUpperCase(),
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontFamily: 'petita',
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.0),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
