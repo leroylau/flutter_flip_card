cf3b4cacd7931272eca4dad2f258cbc3cb41e320
william <luroylau@163.com>
2019-04-11 16:06:35 +0800

    create the Card model

---
+++ lib/models/card.dart
@@ -0,0 +1,20 @@
+class Card {
+  final String backdropAssetPath;
+  final address;
+  final int minHeightInFeet;
+  final int maxHeightInFeet;
+  final double tempInDegrees;
+  final String weatherType;
+  final double windSpeedInMph;
+  final String cardinalDirection;
+
+  Card(
+      this.backdropAssetPath,
+      this.address,
+      this.minHeightInFeet,
+      this.maxHeightInFeet,
+      this.tempInDegrees,
+      this.weatherType,
+      this.windSpeedInMph,
+      this.cardinalDirection);
+}
